4baead215ce6d52b6d0f73b25bd59342ffad87db
Alex Perez <aillera91@gmail.com>
2024-06-14 16:39:00 -0500

    Pantalla de Autor creada y terminada

---
+++ src/views/AutorView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="contenedor">
+        <div class="autor">
+            <img src="/imagenes/systemplus.png" alt="">
+            <p>Este proyecto fue desarrollado por:</p>
+            <h3>Alex Illera</h3>
+            <p>Curso Diseño Web - SystemPlus - 2024</p>
+            <a href="https://www.github.com/aillera91" target="_blank">aillera91</a>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .contenedor {
+        display: flex;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+    }
+    img{
+        max-width: 50%;
+    }
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+</style>
